21fe687a6dd1037c7cefdc07149ff7152709297a
João Paulo Monteiro de Barros Ceva Rodrigues <65778250+joaombc@users.noreply.github.com>
2023-09-14 20:38:37 -0300

    Add files via upload

---
+++ Lista 1/vetor4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void){
+    int n;
+
+    scanf("%d", &n);
+
+    if(n<5 || n>10000){
+        return 1;
+    }
+
+    int v[n];
+    
+    for(int i = 0; i<n; i++){
+        scanf("%d", &v[i]);
+        if(v[i]<-1000000 || v[i]>1000000){
+            return 1;
+        }
+        
+    }
+
+    for(int i = 0; i < n; i++){
+        if(v[i]%2 == 0){
+            printf("%d ", i);
+        }
+
+    }
+
+    printf("\n");
+    for(int i = 0; i < n; i++){
+
+        if(v[i]%2 != 0){
+            printf("%d ", i);
+        }
+
+    }
+    printf("\n");
+
+
+}
