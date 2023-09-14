060ae9850212df17808ea8b81ce4a1832550cc48
João Paulo Monteiro de Barros Ceva Rodrigues <65778250+joaombc@users.noreply.github.com>
2023-09-13 22:07:52 -0300

    Add files via upload

---
+++ Lista 1/bafo.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void){
+
+    int numero, rodadas;
+
+    numero = 1;
+
+    while(1){
+        scanf("%d", &rodadas);
+        if(rodadas == 0){
+            break;
+        }
+
+        int aldo, beto, i;
+        aldo = 0;
+        beto = 0;
+
+        for(i = 0; i < rodadas; i++){
+            int pontosA, pontosB;
+            scanf("%d %d", &pontosA, &pontosB);
+
+            aldo += pontosA;
+            beto += pontosB;
+        }
+        if (aldo > beto){
+            printf("Teste %d\nA\n\n", numero);
+            }
+        else{
+            printf("Teste %d\nB\n\n", numero);
+        }
+        numero++;
+    }
+    return 0;
+
+    // scanf("%d",&rodadas);
+
+    // while(rodadas != 0){
+    //     scanf("%d %d", &a, &b);
+    //     rodadas--;
+    // }
+}
