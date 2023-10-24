18e16c134c1cf3cbdba8fd7b858983d58cf4e636
João Paulo Monteiro de Barros Ceva Rodrigues <65778250+joaombc@users.noreply.github.com>
2023-10-24 14:37:48 -0300

    Update ultrapassa.c

--- Lista 2/ultrapassa.c
+++ Lista 2/ultrapassa.c
@@ -1,35 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct celula {
+    int dado;
+    struct celula* prox, * prev;
+} celula;
+
+void imprime(celula* le) {
+    while (le->prev != NULL) {
+        printf("%d\n", le->dado);
+        le = le->prev;
+    }
+}
 
 int main() {
-    int v[1000];
-    int limite, num;
-    int soma = 0;
-    int ultrapassou = 0;
+    celula* head = malloc(sizeof(celula));
+    head->prox = NULL;
+    celula* last = head;
 
-    int i = 0;
-    while (1) {
-        scanf("%d", &num);
-        if (num == 0) {
-            break;
+    while (true) {
+        celula* node = malloc(sizeof(celula));
+        node->prox = NULL;
+        scanf(" %d", &node->dado);
+
+        if (!node->dado) break;
+        last->prox = node;
+        last = node;
+    }
+
+    int number, sum = 0;
+    scanf("%d", &number);
+
+    celula* top = malloc(sizeof(celula));
+    top->prox = NULL;
+    top->prev = NULL;
+    celula* prev = top;
+    while (head->prox != NULL) {
+        sum += head->prox->dado;
+        if (sum > number) {
+            celula* node = malloc(sizeof(celula));
+            node->prox = NULL;
+
+            node->dado = head->prox->dado;
+            prev->prox = node;
+            node->prev = prev;
+            prev = node;
+
+            sum = 0;
         }
-        v[i] = num;
-        i++;
+        head = head->prox;
     }
-
-    scanf("%d", &limite);
-
-
-    for (int j = 0; j < i; j++) {
-        soma += v[j];
-        if (soma > limite) {
-            ultrapassou = j;
-            soma = 0;
-        }
-    }
-
-    for (int j = ultrapassou; j >= 0; j--) {
-        printf("%d\n", v[j]);
-    }
+    imprime(prev);
 
     return 0;
 }
