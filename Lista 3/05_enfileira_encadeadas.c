9cab618cca8904ef83cf625d0c46eeb04da8afca
João Paulo Monteiro de Barros Ceva Rodrigues <65778250+joaombc@users.noreply.github.com>
2023-10-25 18:59:04 -0300

    Add files via upload

---
+++ Lista 3/05_enfileira_encadeadas.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct celula {
+    int dado;
+    struct celula* prox;
+} celula;
+
+
+
+int enfileira(celula* f, int x) {
+    celula* nova = malloc(sizeof(celula));
+    if (nova == NULL) return NULL;
+
+    nova->prox = f->prox;
+    f->prox = nova;
+    f->dado = x;
+    return nova;
+}
