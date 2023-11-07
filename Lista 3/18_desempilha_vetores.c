ecc53b637c55bf58e488f37461c887d9092bc3bd
João Paulo Monteiro de Barros Ceva Rodrigues <65778250+joaombc@users.noreply.github.com>
2023-11-07 16:29:39 -0300

    Add files via upload

---
+++ Lista 3/18_desempilha_vetores.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef struct pilha{
+	int *dados;
+	int N, topo;
+}pilha;
+
+int desempilha(pilha *p, int *y){
+	if(p == NULL || p->topo ==0){
+		return 0;
+	}
+	*y = p->dados[--p->topo];
+    return 1;
+}
